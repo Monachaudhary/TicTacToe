62a77d858d5dd49cf6e1f8e231b2df683aa532dd
Manu Chaudhary <96776726+Monachaudhary@users.noreply.github.com>
2023-12-26 12:44:11 +0530

    Update TicTac.java

--- TicTac.java
+++ TicTac.java
@@ -38,8 +38,27 @@ public class TicTac {
     }
 
     private static boolean haveWon(char[][] board, char player) {
-        for (int i = 0; i < board.length; i++) {
+        
+            //check the rows
+            for(int row=0;row<board.length;row++){
+                if(board[row][0]==player&&board[row][1]==player&&board[row][2]==player){
+                    return true;
+                }
+            }
+                //check the column
+             for(int col=0;col<board.length;col++){
+                 if(board[0][col]==player&&board[1][col]==player&&board[2][col]==player){
+                     return true;
+                 }
+             }    
+            
 
+        //check diagonal
+        if(board[0][0]==player && board[1][1]==player && board[2][2] == player){
+            return true;
+        }
+        if(board[0][2]==player && board[1][1] ==player && board[2][0]==player){
+            return true;
         }
         return false;
     }
